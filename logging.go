940838d8ebfc72439774653c17b1d019fb3e28a3
Itay Funes <45236616+itayfunes@users.noreply.github.com>
2023-08-14 11:18:30 +0300

    Update logging.go

--- logging.go
+++ logging.go
@@ -1,51 +1,35 @@
-package logging
+package main
 
 import (
 	"fmt"
-	"runtime"
 
 	"github.com/fatih/color"
 )
 
-var green func(a ...interface{}) string
-var red func(a ...interface{}) string
-var purple func(a ...interface{}) string
-var yellow func(a ...interface{}) string
-var cyan func(a ...interface{}) string
-var blue func(a ...interface{}) string
+var (
+	red   = color.New(color.FgRed).SprintFunc()
+	green = color.New(color.FgGreen).SprintFunc()
+	blue  = color.New(color.FgBlue).SprintFunc()
+)
 
-func Goodln(v ...interface{}) {
-	if runtime.GOOS == "linux" {
-		green = color.New(color.FgGreen).SprintFunc()
-		fmt.Print(green("[+] "))
-		fmt.Println(v...)
-	} else {
-		fmt.Print("[+] ")
-		for _, arg := range v {
-			fmt.Print(arg)
-		}
-		fmt.Print("\n")
-	}
+func logWithPrefix(prefix string, colorFunc func(...interface{}) string, message string) {
+	fmt.Printf("[%s] %s\n", colorFunc(prefix), message)
 }
 
-func Badln(v ...interface{}) {
-	if runtime.GOOS == "linux" {
-		red = color.New(color.FgRed).SprintFunc()
-		fmt.Print(red("[-] "))
-		fmt.Println(v...)
-	} else {
-		fmt.Print("[-] ")
-		fmt.Println(v...)
-	}
+func badln(message string) {
+	logWithPrefix("-", red, message)
 }
 
-func Infoln(v ...interface{}) {
-	if runtime.GOOS == "linux" {
-		purple = color.New(color.FgMagenta).SprintFunc()
-		fmt.Print(purple("[*] "))
-		fmt.Println(v...)
-	} else {
-		fmt.Print("[*] ")
-		fmt.Println(v...)
-	}
+func goodln(message string) {
+	logWithPrefix("+", green, message)
+}
+
+func infoln(message string) {
+	logWithPrefix("*", blue, message)
+}
+
+func main() {
+	badln("This is a bad message.")
+	goodln("This is a good message.")
+	infoln("This is an info message.")
 }
